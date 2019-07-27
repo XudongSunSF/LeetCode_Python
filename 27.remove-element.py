5c09e57a818206f935a135dab2a9ce692ecd6992
Xudong Sun <xudongsun@Xudongs-MacBook-Pro-2.local>
2019-07-26 22:40:44 -0700

    27

---
+++ 27.remove-element.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=27 lang=python3
+#
+# [27] Remove Element
+#
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        count = 0 # count non-target elements
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[count] =  nums[i]
+                count += 1
+
+        return count
+
+
